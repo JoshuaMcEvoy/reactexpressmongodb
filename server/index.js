825810979d404ddbcb72ed7570d54cf0f0f0a854
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-01-19 13:12:37 +1100

    setting up secure keys

--- server/index.js
+++ server/index.js
@@ -2,10 +2,18 @@ const express = require('express');
 // same as 'import express from 'express';' but need to use this for server side:
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./keys.js');
+
 
 const app = express();
 
-passport.use( new GoogleStrategy() );
+passport.use( new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken);
+}));
 
 // ROUTES
 app.get('/auth/google', (req, res) => {
