c0c0cd63e35b7a78fac1c1aae66ef22df0adf407
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-01-19 12:18:40 +1100

    specifying node and npm versions

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// same as 'import express from 'express';' but need to use this for server side:
+
+const app = express();
+
+// generating routes
+app.get('/', (req, res) => {
+  res.send({hi: 'there'});
+});
+
+//  getting port from heroku
+const PORT = process.env.PORT || 5000
+
+// listening for the PORT
+app.listen(PORT);
