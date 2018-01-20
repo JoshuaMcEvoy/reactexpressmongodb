4b6dcdf09a077459f337c3dbb7d05fe3cd2b94db
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-01-20 11:24:14 +1100

    remove proxy again

--- server/services/passport.js
+++ server/services/passport.js
@@ -20,8 +20,7 @@ passport.deserializeUser((id, done) => {
 passport.use( new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback',
-  proxy: true
+  callbackURL: '/auth/google/callback'
 },
 (accessToken, refreshToken, profile, done) => {
 
