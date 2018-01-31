76ecf43e0ff4fc52794374407bf2c9387e07ed6c
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-01-31 17:14:57 +1100

    finished survey route

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,8 +1,21 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
 
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      // take the list of email address, split it into an array, and return an object for every email address with the property of email and the value of the actual email address.
+      recipients: recipients.siple(',').map( email => ({ email }) ),
+      _user: req.user.id,
+      dateSend: Date.new()
+    });
   });
 };
