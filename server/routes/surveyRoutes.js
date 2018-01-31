392e9b83860a43f40f1b9e33d04c644934ae2223
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-01-31 17:18:02 +1100

    trim email and fixed split

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -13,7 +13,7 @@ module.exports = app => {
       subject,
       body,
       // take the list of email address, split it into an array, and return an object for every email address with the property of email and the value of the actual email address.
-      recipients: recipients.siple(',').map( email => ({ email }) ),
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSend: Date.new()
     });
