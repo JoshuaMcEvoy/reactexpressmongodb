7acb4b36a35931f33e6070fc52b0e80a4711241c
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-01-31 16:26:27 +1100

    completed servey model with relationship to User model added Recipient model

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema ({
   title: String,
   body: String,
   subject: String,
-  recipients: [String],
+  // nested model - sub-document collection - documents are limited to 4mb
+  recipients: [ RecipientSchema ],
   yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 }
+  no: { type: Number, default: 0 },
+  // relationship to the User model
+  _user: { type: Schema.Type.ObjectId, ref: 'User'},
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
