34b1a5c5007d54539abc425da6a5f022498ca102
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-01-31 15:19:27 +1100

    production pathing for all requests

--- server/index.js
+++ server/index.js
@@ -31,6 +31,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production'){
+  // server up production files.
+  app.use(express.static('client/build'));
+
+  // If we get a request that the server doesn't understand then serve up 'index.html' file.
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+};
 
 //  getting port from heroku
 const PORT = process.env.PORT || 5000
